9f750d2ef79ba7d3a371ad7c1b70c65aed8783a3
Anibal Corral <anibal.corral@gmail.com>
2022-10-18 16:59:02 -0400

    Register, login, and 404 implemented

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styles: [
+  styleUrls: ['./register.component.css'
   ]
 })
 export class RegisterComponent implements OnInit {
