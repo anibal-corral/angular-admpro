d6a51bd6f786f23562446fb62ccd0e2a070946ea
Anibal Corral <anibal.corral@gmail.com>
2022-10-18 16:47:57 -0400

    Routes applied

---
+++ src/app/pages/nopagefound/nopagefound.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nopagefound',
+  templateUrl: './nopagefound.component.html',
+  styles: [
+  ]
+})
+export class NopagefoundComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
