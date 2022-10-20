f35139a0cea84912d0fdb8066eaf24cb08cf4f6f
Anibal Corral <anibal.corral@gmail.com>
2022-10-20 10:37:22 -0400

    especiic path implemented

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,7 +7,7 @@ import { NopagefoundComponent } from './nopagefound/nopagefound.component';
 
 
 const routes: Routes = [
-
+{ path: '', redirectTo: '/dashboard', pathMatch: 'full'},
   { path: '**', component: NopagefoundComponent}
 ]
 
--- src/app/pages/pages-routing.module.ts
+++ src/app/pages/pages-routing.module.ts
@@ -7,14 +7,14 @@ import { Grafica1Component } from './grafica1/grafica1.component';
 
 const routes: Routes = [
   {
-    path: '', 
+    path: 'dashboard', 
     component: PagesComponent,
     children: [
  
-      { path: 'dashboard', component: DashboardComponent},
+      { path: '', component: DashboardComponent},
       { path: 'progress', component: ProgressComponent},
       { path: 'grafica1', component: Grafica1Component},
-      { path: '', redirectTo: '/dashboard', pathMatch: 'full'},
+      // { path: '', redirectTo: '/dashboard', pathMatch: 'full'},
     ]
 },
 ]
