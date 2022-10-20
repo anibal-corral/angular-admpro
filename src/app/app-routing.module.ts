f3a349b4dffa848db4102ebe7f13297ff0e1c82a
Anibal Corral <anibal.corral@gmail.com>
2022-10-20 10:27:26 -0400

    Pages routing created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,31 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { PagesRoutingModule } from './pages/pages-routing.module';
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Grafica1Component } from './pages/grafica1/grafica1.component';
 import { NopagefoundComponent } from './nopagefound/nopagefound.component';
-import { PagesComponent } from './pages/pages.component';
+
 
 const routes: Routes = [
-  {
-    path: '', 
-    component: PagesComponent,
-    children: [
- 
-      { path: 'dashboard', component: DashboardComponent},
-      { path: 'progress', component: ProgressComponent},
-      { path: 'grafica1', component: Grafica1Component},
-      { path: '', redirectTo: '/dashboard', pathMatch: 'full'},
-    ]
-},
-
-
   { path: 'login', component: LoginComponent},
   { path: 'register', component: RegisterComponent},
-
-  
   { path: '**', component: NopagefoundComponent}
 ]
 
@@ -33,7 +16,8 @@ const routes: Routes = [
 @NgModule({
   declarations: [],
   imports: [
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes),
+    PagesRoutingModule
   ],
   exports:[
     RouterModule
---
+++ src/app/pages/pages-routing.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { PagesComponent } from './pages.component';
+import { ProgressComponent } from './progress/progress.component';
+import { Grafica1Component } from './grafica1/grafica1.component';
+
+const routes: Routes = [
+  {
+    path: '', 
+    component: PagesComponent,
+    children: [
+ 
+      { path: 'dashboard', component: DashboardComponent},
+      { path: 'progress', component: ProgressComponent},
+      { path: 'grafica1', component: Grafica1Component},
+      { path: '', redirectTo: '/dashboard', pathMatch: 'full'},
+    ]
+},
+]
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    RouterModule.forChild(routes)
+  ],
+  exports:[
+    RouterModule
+  ]
+})
+export class PagesRoutingModule { }
