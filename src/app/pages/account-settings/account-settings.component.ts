02239b93021df0d03c7d4d59b1ab875afd43851a
Anibal Corral <anibal.corral@gmail.com>
2022-10-21 10:35:04 -0400

    Change theme working

--- src/app/pages/account-settings/account-settings.component.ts
+++ src/app/pages/account-settings/account-settings.component.ts
@@ -9,13 +9,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AccountSettingsComponent implements OnInit {
   linkTheme = document.querySelector('#theme');
+  links!:NodeListOf<Element>;
   constructor() { }
 
   ngOnInit(): void {
+   this.links = document.querySelectorAll('.selector');
+    this.checkCurrentTheme();
   }
   changeTheme(themeName:string){
     const url = `./assets/css/colors/${themeName}.css`;
     this.linkTheme?.setAttribute('href',url);
     localStorage.setItem('theme',url);
+    this.checkCurrentTheme();
   }
+
+  checkCurrentTheme(){
+    this.links.forEach(element => {
+      element.classList.remove('working');
+      const btnTheme = element.getAttribute('data-theme');
+      const btnThemeUrl = `./assets/css/colors/${btnTheme}.css`;
+      const currentTheme = this.linkTheme?.getAttribute('href');
+      if(btnThemeUrl === currentTheme ){
+        element.classList.add('working');
+      }
+  })
+  
+    
+    }
 }
