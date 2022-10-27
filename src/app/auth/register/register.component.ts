1941518a861b3b80627ba97932d36fecd2c12c22
Anibal Corral <anibal.corral@gmail.com>
2022-10-27 12:31:04 -0400

    Register form name,email and terms validated

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -9,20 +9,38 @@ import { FormBuilder, Validators } from '@angular/forms';
 })
 export class RegisterComponent implements OnInit {
 public registerForm = this.fb.group({
-  name:['Doctor Jekyll',[Validators.required, Validators.minLength(3)]],
-  email:['drjekyll@aecs.com',[Validators.required, Validators.email]],
-  pwd:['123',[Validators.required, Validators.minLength(3)]],
-  pwd2:['123',[Validators.required, Validators.minLength(3)]],
+  name:['',[Validators.required, Validators.minLength(3)]],
+  email:['',[Validators.required, Validators.email]],
+  pwd:['',[Validators.required, Validators.minLength(3)]],
+  pwd2:['',[Validators.required, Validators.minLength(3)]],
   terms:[false,[Validators.required]],
 
 })
+formSubmitted = false;
   constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
   }
 
   createUser(){
+    this.formSubmitted = true;
     console.log(this.registerForm.value);
+    if(this.registerForm.valid){
+      console.log('Sending data');
+    }else{
+      console.log("Form is not valid");
+      
+    }
+  }
+validField(field:string){
+  if(this.registerForm.get(field)?.invalid && this.formSubmitted==true){
+    return true;
+  }else{
+    return false;
   }
 
+}
+termsAccepted(){
+  return !this.registerForm.get('terms')?.value && this.formSubmitted;
+}
 }
