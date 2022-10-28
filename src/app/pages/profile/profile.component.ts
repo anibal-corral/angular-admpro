d631967a38b529ebf242bbfd2dc42e13b6f537b3
Anibal Corral <anibal.corral@gmail.com>
2022-10-28 14:53:26 -0400

    File upload profile picture is implemented

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { User } from 'src/app/models/user.model';
 import { UserService } from '../../services/user.service';
+import { FileuploadService } from '../../services/fileupload.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-profile',
@@ -10,23 +13,69 @@ import { UserService } from '../../services/user.service';
 })
 export class ProfileComponent implements OnInit {
 profileForm!:FormGroup;
-  constructor(private fb: FormBuilder, private userService:UserService) { }
+user!:User;
+profilePicture!:File;
+profilePicturePreview:string|ArrayBuffer|null =null;
+
+  constructor(private fb: FormBuilder, private userService:UserService, private fileUploadService:FileuploadService) {
+    this.user = userService.user;
+   }
 
   ngOnInit(): void {
     this.profileForm = this.fb.group({
-      name:['',Validators.required],
-      email:['',[Validators.required, Validators.email]],
+      name:[this.user.name,Validators.required],
+      email:[this.user.email,[Validators.required, Validators.email]],
     })
   }
 
   updateProfile(){
     this.userService.updateProfile(this.profileForm.value).subscribe(
-      resp => {
+      (resp:any )=> {
         console.log(resp);
+        const {email, name, google, role, uid} = resp.user;
+        this.user.email=email;
+        this.user.name=name;
+        this.user.google=google;
+        this.user.role=role;
+        this.user.uid=uid;
+       Swal.fire('Success','Changes saved successfully','success' );
+      },(err)=>{
+        Swal.fire('Error', err.error.msg,'error');
+        
         
       }
     )
     
+    
+  }
+  updateProfilePicture(){
+  this.fileUploadService.updateProfilePicture(this.profilePicture,'users',this.user.uid||'')
+    .then(resp => 
+      this.user.img=resp
+      ).then(()=> Swal.fire('Success','Changes saved successfully','success' )).catch((err)=>
+      Swal.fire('Error', 'Error','error')
+      )
+
+
+  }
+
+  changeImg(file:any){
+  this.profilePicture = file 
+  if(!file){
+    this.profilePicturePreview=null;
+    return;
+  }
+  
+
+  const reader = new FileReader();
+  const url64 = reader.readAsDataURL(file);
+  reader.onloadend =() =>
+  {
+    this.profilePicturePreview = reader.result;
+    //  console.log(reader.result);
+    
+  }
+    // console.log(event);
     
   }
 
--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/models/user.model';
 import { SidebarService } from '../../services/sidebar.service';
 import { UserService } from '../../services/user.service';
 
@@ -10,15 +11,15 @@ import { UserService } from '../../services/user.service';
 })
 export class SidebarComponent implements OnInit {
 menuItems:any[]=[];
-imgUrl:string='';
-userName:string='';
+user!:User;
   constructor(private sidebarService:SidebarService, private userService:UserService) {
     this.menuItems=sidebarService.menu;
    }
 
   ngOnInit(): void {
-    this.imgUrl = this.userService.user.imgUrl;
-    this.userName = this.userService.user.name;
+    // this.imgUrl = this.userService.user.imgUrl;
+    // this.userName = this.userService.user.name;
+    this.user = this.userService.user;
   }
 
 }
