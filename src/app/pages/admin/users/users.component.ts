6e0d615f7e80d77eafb130dca8edb97610c85320
Anibal Corral <anibal.corral@gmail.com>
2022-10-31 11:52:04 -0400

    Users are gotten by pagination

--- src/app/pages/admin/users/users.component.ts
+++ src/app/pages/admin/users/users.component.ts
@@ -11,16 +11,34 @@ import { UserService } from '../../../services/user.service';
 export class UsersComponent implements OnInit {
 totalUsers:number=0;
 users:User[] = [];
+from:number = 0;
   constructor(private userService:UserService) { }
 
   ngOnInit(): void {
-    this.userService.getUsers(0).subscribe(
-      ({total,users})=>
-      {
-        this.totalUsers = total;
-        this.users = users;
+    this.loadUsers();
+    }
+    loadUsers(){
+      this.userService.getUsers(this.from).subscribe(
+        ({total,users})=>
+        {
+          this.totalUsers = total;
+        
+          this.users = users;
+        }
+        )
+    }
+
+    changePage(value:number){
+      this.from += value;
+      //From never could be less than 0
+      if(this.from<0){
+      this.from =0;
+      }else if(this.from >this.totalUsers){
+        this.from -= value;
       }
-      )
+
+      this.loadUsers();
+
     }
 
 }
