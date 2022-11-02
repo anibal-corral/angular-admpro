4158bed4765042e43354df1fb04d6997ded12211
Anibal Corral <anibal.corral@gmail.com>
2022-11-02 10:41:38 -0400

    Global Search Implemented

--- src/app/pages/search/search.component.ts
+++ src/app/pages/search/search.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { SearchsService } from '../../services/searchs.service';
+import { User } from '../../models/user.model';
+import { Doctor } from 'src/app/models/doctor.model';
+import { Hospital } from '../../models/hospital.model';
 
 @Component({
   selector: 'app-search',
@@ -8,11 +12,38 @@ import { ActivatedRoute } from '@angular/router';
   ]
 })
 export class SearchComponent implements OnInit {
+  users:User[]=[];
+  doctors:Doctor[]=[];
+  hospitals:Hospital[]=[];
 
-  constructor(private activatedRoute:ActivatedRoute) { }
+
+  constructor(private activatedRoute:ActivatedRoute, private searchService:SearchsService, private router:Router) { }
 
   ngOnInit(): void {
-    this.activatedRoute.params.subscribe(({term})=>console.log(term));
+    this.activatedRoute.params.subscribe(({term})=>this.globalSearch(term));
   }
+  
+globalSearch(term:string){
+  this.searchService.gobalSearch(term).subscribe((resp:any)=>
+    {
+      // console.log(resp);
+      this.users=resp.users;
+      this.doctors=resp.doctors;
+      this.hospitals=resp.hospitals;
+    }
+    
+    
+    );
 
+}
+
+openDoctor(doctor:Doctor){
+this.router.navigateByUrl(`/dashboard/doctors/${doctor.uid}`)
+}
+openHospital(hospital:Hospital){
+  
+}
+openUser(user:User){
+  
+}
 }
